73ea711ab4deff180c3f09647987fd3f5061e49e
Suvadra-Barua <suvadra@universalmachine.io>
2025-12-13 00:05:44 +0600

    Change deploy script

--- ignition/modules/deploy.ts
+++ ignition/modules/deploy.ts
@@ -1,8 +1,10 @@
 import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
+const { vars } = require("hardhat/config");
 
 const DeployModule = buildModule("DeployAdapterManager", (m) => {
-  // Get admin address parameter or use deployer account as default
-  const admin = m.getParameter("SAFE_ADMIN", m.getAccount(0));
+  // Get admin address from Hardhat vars, parameter, or use deployer account as default
+  const admin = vars.get("SAFE_ADMIN") || m.getParameter("SAFE_ADMIN");
+  console.log("🔧 Deploying AdapterManager with admin:", admin);
   // Deploy AdapterManager directly with constructor
   const adapterManager = m.contract("AdapterManager", [admin], {
     id: "AdapterManager",
